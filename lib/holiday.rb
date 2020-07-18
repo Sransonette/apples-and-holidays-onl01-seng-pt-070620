e16f5e0817b0db51bc1adecc17174729c2b6f674
Sean Ransonette <sransonette@gmail.com>
2020-07-18 05:21:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.map do |season, holiday|
   holiday.map do |holiday, event|
-    holiday.
+    holiday if supply.include? 
   end
 end
 end
